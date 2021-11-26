6f606aca36ea217b9fd4588aebb42841e27fd688
DavidReider <freakdj1997@yahoo.com>
2021-11-26 09:52:40 -0500

    Add Simple Header Component

--- floor-track/src/App.js
+++ floor-track/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Footer />
     </>
   );
---
+++ floor-track/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div class="title">
+        <a href="/"> Solana Floor Tracker </a>
+      </div>
+    </header>
+  );
+}
